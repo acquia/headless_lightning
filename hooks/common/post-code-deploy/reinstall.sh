7e2fddbe6340af021c97900f649e77c443eaa0b2
Adam Balsam <adam.balsam@acquia.com>
2017-07-28 12:09:37 -0400

    include site name when installing

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -8,4 +8,4 @@ site="$1"
 target_env="$2"
 
 # Fresh install of Headless Lightning.
-drush @$site.$target_env site-install headless_lightning --yes --account-pass=admin
+drush @$site.$target_env site-install headless_lightning --yes --account-pass=admin --site-name='Headless Lightning - Nightly Build'
