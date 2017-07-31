1cdf2b7ea28beb6eaa2a5301bf5b83edf2222ca5
Adam Hoenich <adam.hoenich@acquia.com>
2017-07-31 11:39:09 -0400

    Created headless_ui module.

---
+++ modules/headless_ui/tests/src/Functional/UiTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\Tests\headless_ui\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+class UiTest extends BrowserTestBase {
+
+  protected $profile = 'lightning_headless';
+
+  public function test() {
+    $assert = $this->assertSession();
+
+    $account = $this->drupalCreateUser([], NULL, TRUE);
+    $this->drupalLogin($account);
+
+    $node_type = $this->drupalCreateContentType();
+    $this->assertSame(DRUPAL_DISABLED, $node_type->getPreviewMode());
+    $this->assertFalse($node_type->displaySubmitted());
+
+    $this->drupalGet('/admin/structure/types/add');
+    $assert->statusCodeEquals(200);
+    $assert->fieldNotExists('display_submitted');
+    $assert->fieldNotExists('options[promote]');
+    $assert->fieldNotExists('options[sticky]');
+    $assert->fieldNotExists('preview_mode');
+
+    $this->drupalGet('/admin/structure/types/manage/' . $node_type->id());
+    $assert->statusCodeEquals(200);
+    $assert->linkNotExists('Manage display');
+
+    $this->drupalGet('/admin/structure');
+    $assert->statusCodeEquals(200);
+    $assert->linkNotExists('Block layout');
+    $assert->linkNotExists('Display modes');
+  }
+
+}
