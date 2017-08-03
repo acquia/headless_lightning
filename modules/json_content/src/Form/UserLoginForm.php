e314fbc812e65a6cd778ee534d4f95d5a8fde530
Adam Balsam <adam.balsam@acquia.com>
2017-08-03 14:46:47 -0400

    Use logic built into frontpage controller to handle what happens after login

--- modules/json_content/src/Form/UserLoginForm.php
+++ modules/json_content/src/Form/UserLoginForm.php
@@ -5,9 +5,6 @@ namespace Drupal\json_content\Form;
 use Drupal\user\Form\UserLoginForm as UserLoginFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-/**
- * Overrides the redirect of the login form to
- */
 class UserLoginForm extends UserLoginFormBase {
 
   /**
@@ -15,7 +12,7 @@ class UserLoginForm extends UserLoginFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $account = $this->userStorage->load($form_state->get('uid'));
-    $form_state->setRedirect('view.content.page_1');
+    $form_state->setRedirect('<front>');
     user_login_finalize($account);
   }
 
