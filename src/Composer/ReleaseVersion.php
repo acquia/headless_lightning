2e3b256bde715599c673bd5b8182fa0545077b03
Adam Balsam <adam.balsam@acquia.com>
2017-08-15 16:54:53 -0400

    Add support for updating version strings

---
+++ src/Composer/ReleaseVersion.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Acquia\headless_lightning\Composer;
+
+use Acquia\Lightning\Composer\ReleaseVersion as BaseReleaseVersion;
+use Composer\Script\Event;
+
+/**
+ * Updates the version number in Lightning's component info files.
+ */
+class ReleaseVersion extends BaseReleaseVersion {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function execute(Event $event) {
+    parent::execute($event);
+  }
+
+  /**
+   * No makefiles in Headless Lightning
+   */
+  protected static function updateMakeFile($version) {}
+
+}
