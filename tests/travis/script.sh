8e8fcf2ca3ea7253490c3fae33afe2cfed4b54d7
TravisCarden <travis.carden@acquia.com>
2019-05-16 14:11:41 -0400

    Updated ORCA implementation.

---
+++ tests/travis/script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# NAME
+#     script.sh - Run tests
+#
+# SYNOPSIS
+#     script.sh
+#
+# DESCRIPTION
+#     Runs static code analysis and automated tests.
+
+cd "$(dirname "$0")"; source _includes.sh
+
+orca fixture:status
+orca tests:run --sut ${ORCA_SUT_NAME} --sut-only
